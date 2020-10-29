48727c8dc92d23769805280017b5bc88ce029d89
Ahmad <benos.tarabain@gmail.com>
2020-10-29 15:32:41 +0200

    added get user IP

--- routes/poll.js
+++ routes/poll.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/Vote");
+const ip = require("ip");
 
 router.get("/", (req, res) => {
   res.send("Polls");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const vote = req.body.OS;
-  console.log(vote);
+  const userIP = await ip.address();
+
+  const newVote = new User({
+    vote: vote,
+    ipAddress: Number(userIP),
+  });
+
   res.redirect("/");
 });
 
